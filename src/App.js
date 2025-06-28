82d57e05a424ae4cec3e1af34a17c04a211bd2f2
Dev-Im-Imran <imranimran16116@email.com>
2025-06-28 01:41:25 -0700

    src/App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import Navbar from "./Header.js";
-import './App.css';
+import "./App.css";
 import ContentShow from "./ProductDIv.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import CreateOption from './CreateOption.js';
-import Home from './Home.js';
-
-
+import CreateOption from "./CreateOption.js";
+import Home from "./Home.js";
 
 function App() {
-
-  
-
-
   return (
     <>
-    <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route path='/CreateOption' element={<CreateOption />} />
-      <Route path="/" element={<Home />} />
-      <Route path="/ProductDiv" element={<ContentShow />} />
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/CreateOption" element={<CreateOption />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/ProductDiv" element={<ContentShow />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
